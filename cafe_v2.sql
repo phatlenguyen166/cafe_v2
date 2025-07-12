eb244f0edcc5491c2f0c8303a09ca9483f406a58
Hoang Phat <phatlenguyen166@gmail.com>
2025-07-12 18:45:49 +0700

    update: sql

--- cafe_v2.sql
+++ cafe_v2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `cafe_v2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cafe_v2`;
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: localhost    Database: cafe_v2
@@ -40,7 +42,7 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (1,_binary '\0',NULL,'$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','ADMIN','admin'),(2,_binary '\0','https://dummyimage.com/764x563','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','user'),(3,_binary '\0','https://dummyimage.com/780x257','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','phamminhduc'),(4,_binary '\0','https://placekitten.com/663/539','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','lethihong'),(5,_binary '','https://placekitten.com/105/575','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','hodangkhoa'),(6,_binary '','https://www.lorempixel.com/883/251','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','vuthanhcong'),(7,_binary '','https://dummyimage.com/779x129','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','tranthingoc'),(8,_binary '','https://www.lorempixel.com/644/577','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','nguyenba'),(9,_binary '','https://placekitten.com/54/139','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','phamthithuy'),(10,_binary '','https://dummyimage.com/368x441','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','buiquanghuy');
+INSERT INTO `accounts` VALUES (1,_binary '\0','https://dummyimage.com/764x563','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','ADMIN','admin'),(2,_binary '\0','https://dummyimage.com/764x563','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','user'),(3,_binary '\0','https://dummyimage.com/780x257','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','phamminhduc'),(4,_binary '\0','https://placekitten.com/663/539','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','lethihong'),(5,_binary '\0','https://placekitten.com/105/575','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','hodangkhoa'),(6,_binary '\0','https://www.lorempixel.com/883/251','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','vuthanhcong'),(7,_binary '\0','https://dummyimage.com/779x129','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','tranthingoc'),(8,_binary '\0','https://www.lorempixel.com/644/577','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','nguyenba'),(9,_binary '\0','https://placekitten.com/54/139','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','phamthithuy'),(10,_binary '\0','https://dummyimage.com/368x441','$2a$08$trXNcE0TsX2EkmNQo/LeVuoyvj0fQgSa5g9ZclKkY7gavtysjZzZG','USER','buiquanghuy');
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,13 +90,12 @@ CREATE TABLE `equipment` (
   `equipment_id` int NOT NULL AUTO_INCREMENT,
   `is_deleted` bit(1) DEFAULT NULL,
   `equipment_name` varchar(255) DEFAULT NULL,
-  `notes` varchar(255) DEFAULT NULL,
   `purchase_date` date DEFAULT NULL,
-  `purchase_price` double DEFAULT NULL,
+  `purchase_price` decimal(15,0) DEFAULT NULL,
   `quantity` int DEFAULT NULL,
-  `total_amount` double DEFAULT NULL,
+  `total_amount` decimal(15,0) DEFAULT NULL,
   PRIMARY KEY (`equipment_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +104,7 @@ CREATE TABLE `equipment` (
 
 LOCK TABLES `equipment` WRITE;
 /*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
-INSERT INTO `equipment` VALUES (12,_binary '\0','Máy pha cà phê','','2025-07-01',17000000,1,NULL);
+INSERT INTO `equipment` VALUES (12,_binary '\0','Máy pha cà phê','2025-07-01',17000000,2,34000000),(13,_binary '\0','a2','1111-01-01',400000,10,4000000),(15,_binary '\0','Máy rửa chén','2025-07-10',17000000,2,34000000),(16,_binary '\0','Hello','2025-07-10',2000000,1,2000000),(17,_binary '\0','Quạt máy lớn','2025-07-10',300000,5,1500000);
 /*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -182,7 +183,7 @@ CREATE TABLE `imports` (
   `is_deleted` bit(1) DEFAULT NULL,
   `import_date` date DEFAULT NULL,
   `quantity` int DEFAULT NULL,
-  `total_amount` double DEFAULT NULL,
+  `total_amount` decimal(15,0) DEFAULT NULL,
   `employee_id` int DEFAULT NULL,
   `product_id` int DEFAULT NULL,
   PRIMARY KEY (`imports_id`),
@@ -212,7 +213,7 @@ DROP TABLE IF EXISTS `invoice_details`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `invoice_details` (
   `is_deleted` bit(1) DEFAULT NULL,
-  `price_at_sale_time` double DEFAULT NULL,
+  `price_at_sale_time` decimal(15,0) DEFAULT NULL,
   `quantity` int DEFAULT NULL,
   `invoice_id` int NOT NULL,
   `menu_item_id` int NOT NULL,
@@ -229,7 +230,7 @@ CREATE TABLE `invoice_details` (
 
 LOCK TABLES `invoice_details` WRITE;
 /*!40000 ALTER TABLE `invoice_details` DISABLE KEYS */;
-INSERT INTO `invoice_details` VALUES (_binary '\0',50000,5,176,1);
+INSERT INTO `invoice_details` VALUES (_binary '\0',50000,5,176,1),(_binary '\0',50000,5,177,1),(_binary '\0',50,10,178,1),(_binary '\0',50,2,178,2),(_binary '\0',50000,2,179,1),(_binary '\0',40000,2,179,10);
 /*!40000 ALTER TABLE `invoice_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -244,13 +245,13 @@ CREATE TABLE `invoices` (
   `invoice_id` int NOT NULL AUTO_INCREMENT,
   `created_at` datetime(6) DEFAULT NULL,
   `is_deleted` bit(1) DEFAULT NULL,
-  `total_amount` double DEFAULT NULL,
+  `total_amount` decimal(15,0) DEFAULT NULL,
   `promotion_id` int DEFAULT NULL,
   `status` enum('UNPAID','PAID','MERGE_TABLE','CANCELLED') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`invoice_id`),
   KEY `FKsev3d3jlk86toj8l50myia2ts` (`promotion_id`),
   CONSTRAINT `FKsev3d3jlk86toj8l50myia2ts` FOREIGN KEY (`promotion_id`) REFERENCES `promotions` (`promotion_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=177 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=180 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -259,7 +260,7 @@ CREATE TABLE `invoices` (
 
 LOCK TABLES `invoices` WRITE;
 /*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
-INSERT INTO `invoices` VALUES (176,'2025-07-03 18:14:10.858156',_binary '\0',250000,NULL,'PAID');
+INSERT INTO `invoices` VALUES (176,'2025-07-03 18:14:10.858156',_binary '\0',250000,NULL,'PAID'),(177,'2025-07-10 22:33:55.377940',_binary '\0',250000,NULL,'UNPAID'),(178,'2025-07-10 22:58:32.670682',_binary '\0',600,NULL,'PAID'),(179,'2025-07-10 23:01:32.272464',_binary '\0',180000,NULL,'UNPAID');
 /*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,7 +303,7 @@ DROP TABLE IF EXISTS `menu_items`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `menu_items` (
   `menu_item_id` int NOT NULL AUTO_INCREMENT,
-  `current_price` double DEFAULT NULL,
+  `current_price` decimal(15,0) DEFAULT NULL,
   `is_deleted` bit(1) DEFAULT NULL,
   `item_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`menu_item_id`)
@@ -330,7 +331,7 @@ CREATE TABLE `positions` (
   `position_id` int NOT NULL AUTO_INCREMENT,
   `is_deleted` bit(1) DEFAULT NULL,
   `position_name` varchar(255) DEFAULT NULL,
-  `salary` double DEFAULT NULL,
+  `salary` int DEFAULT NULL,
   PRIMARY KEY (`position_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -341,7 +342,7 @@ CREATE TABLE `positions` (
 
 LOCK TABLES `positions` WRITE;
 /*!40000 ALTER TABLE `positions` DISABLE KEYS */;
-INSERT INTO `positions` VALUES (1,_binary '','Quản lý quán cà phê',12000000),(2,_binary '\0','Nhân viên pha chế',8000000),(3,_binary '\0','Nhân viên phục vụ',7000000),(4,_binary '','Kế toán trưởng',10000000),(5,_binary '\0','Nhân viên thu ngân',7500000),(6,_binary '\0','Nhân viên vệ sinh',6000000),(7,_binary '\0','Nhân viên marketing',9000000),(8,_binary '\0','Nhân viên giao hàng',7000000),(9,_binary '\0','Trưởng ca',9500000),(10,_binary '\0','Nhân viên kho',6500000);
+INSERT INTO `positions` VALUES (1,_binary '\0','Quản lý quán cà phê',1200000000),(2,_binary '\0','Nhân viên pha chế',8000000),(3,_binary '\0','Nhân viên phục vụ',7000000),(4,_binary '\0','Kế toán trưởng',10000000),(5,_binary '\0','Nhân viên thu ngân',7500000),(6,_binary '\0','Nhân viên vệ sinh',6000000),(7,_binary '\0','Nhân viên marketing',9000000),(8,_binary '\0','Nhân viên giao hàng',7000000),(9,_binary '\0','Trưởng ca',9500000),(10,_binary '\0','Nhân viên kho',6500000);
 /*!40000 ALTER TABLE `positions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -358,7 +359,7 @@ CREATE TABLE `products` (
   `product_name` varchar(255) DEFAULT NULL,
   `quantity` int DEFAULT NULL,
   `unit_id` int DEFAULT NULL,
-  `price` double DEFAULT NULL,
+  `price` decimal(15,0) DEFAULT NULL,
   PRIMARY KEY (`product_id`),
   KEY `FKeex0i50vfsa5imebrfdiyhmp9` (`unit_id`),
   CONSTRAINT `FKeex0i50vfsa5imebrfdiyhmp9` FOREIGN KEY (`unit_id`) REFERENCES `units` (`unit_id`)
@@ -466,7 +467,7 @@ CREATE TABLE `table_reservations_detail` (
 
 LOCK TABLES `table_reservations_detail` WRITE;
 /*!40000 ALTER TABLE `table_reservations_detail` DISABLE KEYS */;
-INSERT INTO `table_reservations_detail` VALUES (NULL,NULL,_binary '\0','2025-07-05 18:14:10.945291',1,176,18);
+INSERT INTO `table_reservations_detail` VALUES (NULL,NULL,_binary '\0','2025-07-05 18:14:10.945291',1,176,18),(NULL,NULL,_binary '\0','2025-07-10 22:33:55.425655',1,177,17),(NULL,NULL,_binary '\0','2025-07-10 22:58:32.745175',1,178,19),(NULL,NULL,_binary '\0','2025-07-10 23:01:32.278466',1,179,20);
 /*!40000 ALTER TABLE `table_reservations_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -492,7 +493,7 @@ CREATE TABLE `tables` (
 
 LOCK TABLES `tables` WRITE;
 /*!40000 ALTER TABLE `tables` DISABLE KEYS */;
-INSERT INTO `tables` VALUES (1,_binary '\0','Bàn 1','AVAILABLE'),(2,_binary '\0','Bàn 2','AVAILABLE'),(3,_binary '\0','Bàn 3','AVAILABLE'),(4,_binary '\0','Bàn 4','AVAILABLE'),(5,_binary '\0','Bàn 5','AVAILABLE'),(6,_binary '\0','Bàn 6','AVAILABLE'),(7,_binary '\0','Bàn 7','AVAILABLE'),(8,_binary '\0','Bàn 8','AVAILABLE'),(9,_binary '\0','Bàn 9','AVAILABLE'),(10,_binary '\0','Bàn 10','AVAILABLE'),(11,_binary '\0','Bàn 11','AVAILABLE'),(12,_binary '\0','Bàn 12','AVAILABLE'),(13,_binary '\0','Bàn 13','AVAILABLE'),(14,_binary '\0','Bàn 14','AVAILABLE'),(15,_binary '\0','Bàn 15','AVAILABLE'),(16,_binary '\0','Bàn 16','AVAILABLE'),(17,_binary '\0','Bàn 17','AVAILABLE'),(18,_binary '\0','Bàn 18','AVAILABLE'),(19,_binary '\0','Bàn 19','AVAILABLE'),(20,_binary '\0','Bàn 20','AVAILABLE');
+INSERT INTO `tables` VALUES (1,_binary '\0','Bàn 1','AVAILABLE'),(2,_binary '\0','Bàn 2','AVAILABLE'),(3,_binary '\0','Bàn 3','AVAILABLE'),(4,_binary '\0','Bàn 4','AVAILABLE'),(5,_binary '\0','Bàn 5','AVAILABLE'),(6,_binary '\0','Bàn 6','AVAILABLE'),(7,_binary '\0','Bàn 7','AVAILABLE'),(8,_binary '\0','Bàn 8','AVAILABLE'),(9,_binary '\0','Bàn 9','AVAILABLE'),(10,_binary '\0','Bàn 10','AVAILABLE'),(11,_binary '\0','Bàn 11','AVAILABLE'),(12,_binary '\0','Bàn 12','AVAILABLE'),(13,_binary '\0','Bàn 13','AVAILABLE'),(14,_binary '\0','Bàn 14','AVAILABLE'),(15,_binary '\0','Bàn 15','AVAILABLE'),(16,_binary '\0','Bàn 16','AVAILABLE'),(17,_binary '\0','Bàn 17','OCCUPIED'),(18,_binary '\0','Bàn 18','AVAILABLE'),(19,_binary '\0','Bàn 19','AVAILABLE'),(20,_binary '\0','Bàn 20','OCCUPIED');
 /*!40000 ALTER TABLE `tables` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -530,4 +531,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-07 15:43:10
+-- Dump completed on 2025-07-12 18:17:00
